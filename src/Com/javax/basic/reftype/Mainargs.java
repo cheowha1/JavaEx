9df177f2ef7d23a23dab99fdbedc37916d3552eb
cheowha1 <cheowha1@naver.com>
2024-11-06 10:53:22 +0900

    main 메서드 매개변수

---
+++ src/Com/javax/basic/reftype/Mainargs.java
@@ -0,0 +1,13 @@
+package Com.javax.basic.reftype;
+
+import java.util.Arrays;
+
+public class Mainargs {
+
+	public static void main(String[] args) {
+		// 메개변수 목록이 args로 전달된다
+		System.out.println("전달된 메개 변수: " + Arrays.toString(args));
+
+	}
+
+}
