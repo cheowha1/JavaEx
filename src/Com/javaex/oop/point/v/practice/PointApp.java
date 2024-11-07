9e1571c118820cba52e24c5d235f5f3f27cbc5c4
cheowha1 <cheowha1@naver.com>
2024-11-07 15:08:23 +0900

    블록 리스트

---
+++ src/Com/javaex/oop/point/v/practice/PointApp.java
@@ -0,0 +1,28 @@
+package Com.javaex.oop.point.v.practice;
+
+//class Song{
+//	// 필드
+//	private String title;
+//	private String artist;
+//	private String album;
+//	private String composer;
+//	private int year;
+//	private int track;
+//	
+//	public Song(String title, String artist, String album, String composer, int year, int track) {
+//		
+//	}
+//	
+//	// getter/setter
+//	public String tittle() {
+//		return tittle;
+//	}
+//}
+
+//public class PointApp {
+//
+//	public static void main(String[] args) {
+//
+//	}
+
+//}
---
+++ src/Com/javaex/oop/staticmember/staticExApp.java
@@ -0,0 +1,10 @@
+package Com.javaex.oop.staticmember;
+
+public class staticExApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
