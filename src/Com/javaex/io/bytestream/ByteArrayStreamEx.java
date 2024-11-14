99b2881dd01cff5eedb1959f388a1a2c3c843e1b
cheowha1 <cheowha1@naver.com>
2024-11-14 11:15:01 +0900

    io

---
+++ src/Com/javaex/io/bytestream/ByteArrayStreamEx.java
@@ -0,0 +1,42 @@
+package Com.javaex.io.bytestream;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class ByteArrayStreamEx {
+
+	public static void main(String[] args) {
+		// 입력 소스
+		byte[]	inSrc = { 0,1 , 2, 3, 4, 5, 6, 7, 8 ,9};
+		//출력 타겟
+		byte[]	outSrc = null;
+		
+		System.out.println("입력 소스:" + Arrays.toString(inSrc));
+		// 스트링 개방
+		try (
+				// Auto Closeable
+				InputStream bis = new ByteArrayInputStram(inSrc);
+				ByteArrayOutputStream bos = new ByteArrayOutputStream();
+	){
+			int data = 0;   // 입력 스트릡으로부터 넘어오는 데이터 저장 변수
+			
+			while((data = bis.read()) != -1) {
+				System.out.println("Read data:" + Integer.toBinaryString(data));
+				bos.write(data);
+			}
+			
+			outSrc = ((ByteArrayOutputStream)bos).toByteArray();
+			// 다운 캐스팅 후 배열로 변환 작업 수행
+			System.out.println(Arrays.toString(outSrc));
+		}catch (IOException e) {
+			e.printStackTrace();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+
+	}
+
+}
